4721c2c3db2af90c41f1427671ed8a1fe762ab9e
Ahmad Rusdi <woat@users.noreply.github.com>
2018-05-30 09:16:05 -0400

    header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <section class="hero is-small is-info">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title is-1">
+            Zip Code Wilmington Labs
+          </h1>
+        </div>
+      </div>
+    </section>
     <section class="hero is-small is-light">
       <div class="hero-body">
         <div class="container">
